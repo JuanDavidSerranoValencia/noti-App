b75961fe76514802e01bd96cb8a25a9d5303e6f4
SerranoValenciaJuanDavid <serranovalenciajuandavid949@gmail.com>
2023-10-13 11:42:22 -0500

    feat:DbSet Entidades

--- Infrastructure/Data/NotiAppContext.cs
+++ Infrastructure/Data/NotiAppContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
@@ -12,13 +13,21 @@ public class NotiAppContext : DbContext
     public NotiAppContext(DbContextOptions options) : base(options)
     {
     }
-    
+    public DbSet<GenericoVsSubmodulo> GenericosVsSubmodulos { get; set; }
+    public DbSet<MaestroVsSubmodulo> MaestrosVsSubmodulos { get; set; }
+    public DbSet<ModuloMaestro> ModulosMaestros { get; set; }
+    public DbSet<PermisoGenerico> PermisosGenericos { get; set; }
+    public DbSet<Rol> Roles { get; set; }
+
+    public DbSet<RolMaestro> RolesMaestros { get; set; }
+
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-         /*modelBuilder.Entity<Cliente>()
-        .HasOne(a => a.ClienteDireccion)
-        .WithOne(b => b.Clientes)
-        .HasForeignKey<ClienteDireccion>(b => b.IdCliente);*/
+        /*modelBuilder.Entity<Cliente>()
+       .HasOne(a => a.ClienteDireccion)
+       .WithOne(b => b.Clientes)
+       .HasForeignKey<ClienteDireccion>(b => b.IdCliente);*/
 
         base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
