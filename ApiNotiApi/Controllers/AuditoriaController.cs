668bd00f4a59e46019fc0e7d1e6b815ed4f5901e
JuanDavidSerranoValencia <serranovalenciajuandavid949@gmail.com>
2023-10-18 16:30:30 -0500

    feat:Contoladores Entidades

--- ApiNotiApi/Controllers/AuditoriaController.cs
+++ ApiNotiApi/Controllers/AuditoriaController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ApiNotiApi.Dtos;
 using AutoMapper;
+using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -29,6 +30,61 @@ public class AuditoriaController : BaseControllerApi
         var auditoria = await _unitOfWork.Auditorias.GetAllAsync();
         /* return Ok(auditors); */
         return _mapper.Map<List<AuditoriaDto>>(auditoria);
+    }
+     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<Auditoria>> Post(AuditoriaDto auditoriaDtos)
+    {
+        var auditoria = _mapper.Map<Auditoria>(auditoriaDtos);
+        this._unitOfWork.Auditorias.Add(auditoria);
+        await _unitOfWork.SaveAsync();
+        if (auditoria == null)
+        {
+            return BadRequest();
+        }
+        auditoriaDtos.Id = auditoria.Id;
+        return CreatedAtAction(nameof(Post), new { id = auditoriaDtos.Id }, auditoriaDtos);
+    }
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<AuditoriaDto>> Get(int id)
+    {
+        var auditoria = await _unitOfWork.Auditorias.GetByIdAsync(id);
+        if (auditoria == null){
+            return NotFound();
+        }
+        return _mapper.Map<AuditoriaDto>(auditoria);
+    }
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<AuditoriaDto>> Put(int id, [FromBody]AuditoriaDto  auditoriaDtos)
+    {
+        if ( auditoriaDtos == null)
+            return NotFound();
+        var auditoria = _mapper.Map<Auditoria>( auditoriaDtos);
+        _unitOfWork.Auditorias.Update(auditoria);
+        await _unitOfWork.SaveAsync();
+        return auditoriaDtos;
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var auditoria = await _unitOfWork.Auditorias.GetByIdAsync(id);
+        if (auditoria == null)
+        {
+            return NotFound();
+        }
+        _unitOfWork.Auditorias.Remove(auditoria);
+        await _unitOfWork.SaveAsync();
+        return NoContent();
     }
 
 
